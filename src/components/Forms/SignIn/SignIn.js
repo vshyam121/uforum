ad55c9c4e9f5de039dfde7a67575cb14b635f6e7
vshyam121 <v.shyam121@gmail.com>
2020-08-11 16:46:23 -0700

    Sign in and sign up components

---
+++ src/components/Forms/SignIn/SignIn.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import './SignIn.scss';
+import Button from '../../Theme/Button/Button';
+import { ClipLoader } from 'react-spinners';
+import { Redirect } from 'react-router-dom';
+import Form from '../../Theme/Form/Form';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+/* Sign in form */
+const SignIn = (props) => {
+  let form = (
+    <React.Fragment>
+      <Form
+        {...props}
+        onSubmit={props.handleSubmit}
+        updateForm={props.updateForm}
+      />
+      <h2 className='form-component__title'>Don't have an account?</h2>
+      <div className='signup'>
+        <Link to='/signup'>
+          <Button>Sign Up</Button>
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+  if (props.loadingUser) {
+    form = (
+      <div className='spinner'>
+        <ClipLoader size={50} />
+      </div>
+    );
+  }
+
+  let errorMessage = null;
+  let error = null;
+  if (props.error) {
+    if (props.error.status === 500) {
+      error = 'Internal server error.';
+    } else {
+      error = props.error.data.message;
+    }
+    errorMessage = <div className='form-component__error'>{error}</div>;
+  }
+
+  let redirect = null;
+  if (props.isAuthenticated) {
+    redirect = <Redirect to='/' />;
+  }
+
+  return (
+    <div className='form-container'>
+      <div className='form-component'>
+        <h2 className='form-component__title'>
+          Please sign in to your account
+        </h2>
+        {redirect}
+        {errorMessage}
+        {form}
+      </div>
+    </div>
+  );
+};
+
+SignIn.propTypes = {
+  loadingUser: PropTypes.bool,
+  error: PropTypes.object,
+  isAuthenticated: PropTypes.string,
+};
+
+export default SignIn;
---
+++ src/components/Forms/SignUp/SignUp.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { ClipLoader } from 'react-spinners';
+import { Redirect } from 'react-router-dom';
+import Form from '../../Theme/Form/Form';
+import PropTypes from 'prop-types';
+
+/* User sign up form */
+const SignUp = (props) => {
+  let form = (
+    <Form
+      {...props}
+      onSubmit={props.handleSubmit}
+      updateForm={props.updateForm}
+    />
+  );
+  if (props.loadingUser) {
+    form = (
+      <div className='spinner'>
+        <ClipLoader size={50} />
+      </div>
+    );
+  }
+
+  let errorMessage = null;
+  let error = null;
+  if (props.error) {
+    if (props.error.status === 500) {
+      error = 'Internal server error.';
+    } else if (props.error.data.message === 'Duplicate field value entered') {
+      error = 'The email you entered is already taken.';
+    } else {
+      error = props.error.data.message;
+    }
+    errorMessage = <div className='form-component__error'>{error}</div>;
+  }
+
+  let redirect = null;
+  if (props.isAuthenticated) {
+    if (props.location.fromCheckout) {
+      redirect = (
+        <Redirect
+          to={{ pathname: '/checkout/order-type', fromSignUp: 'true' }}
+        />
+      );
+    } else {
+      redirect = <Redirect to={{ pathname: '/', fromSignUp: 'true' }} />;
+    }
+  }
+
+  return (
+    <div className='form-container'>
+      <div className='form-component'>
+        <h2 className='form-component__title'>Sign up for an account</h2>
+        {redirect}
+        {errorMessage}
+        {form}
+      </div>
+    </div>
+  );
+};
+
+SignUp.propTypes = {
+  loadingUser: PropTypes.bool,
+  error: PropTypes.object,
+  isAuthenticated: PropTypes.string,
+};
+
+export default SignUp;
