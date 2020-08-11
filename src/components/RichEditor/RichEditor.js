d1d2dcd626e5c3ea32416c3bbbc4f70d1926cb6a
vshyam121 <v.shyam121@gmail.com>
2020-08-11 16:37:40 -0700

    Rich text editor from draft js

--- src/components/RichEditor/RichEditor.js
+++ src/components/RichEditor/RichEditor.js
@@ -1,38 +1,128 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { EditorState, RichUtils, convertToRaw, convertFromRaw } from 'draft-js';
 import './RichEditor.scss';
 import { Editor } from 'draft-js';
 import InlineStyleControls from './InlineStyleControls';
 import BlockStyleControls from './BlockStyleControls';
 
+export const THREAD = 'NEW_THREAD';
+export const REPLY = 'REPLY';
 const RichEditor = (props) => {
+  const { type, readOnly, content } = props;
+
+  useEffect(() => {
+    let initialContent = null;
+    if (content) {
+      initialContent = EditorState.createWithContent(
+        convertFromRaw(JSON.parse(content))
+      );
+    } else {
+      initialContent = EditorState.createEmpty();
+    }
+    setEditorState(initialContent);
+  }, [content]);
+
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+
+  const [contentHasChanged, setContentHasChanged] = useState(false);
+
+  const onChange = (newEditorState) => {
+    if (
+      editorState.getCurrentContent() !== newEditorState.getCurrentContent()
+    ) {
+      setContentHasChanged(true);
+    }
+    setEditorState(newEditorState);
+  };
+
+  const handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(command);
+    if (newState) {
+      onChange(newState);
+    }
+  };
+
+  const onTab = (e) => {
+    const maxDepth = 4;
+    onChange(RichUtils.onTab(e, editorState, maxDepth));
+  };
+
+  const toggleInlineStyle = (inlineStyle) => {
+    onChange(RichUtils.toggleInlineStyle(editorState, inlineStyle));
+  };
+
+  const toggleBlockType = (blockStyle) => {
+    onChange(RichUtils.toggleBlockType(editorState, blockStyle));
+  };
+
   const editor = useRef(null);
   const focus = () => {
     editor.current.focus();
   };
 
-  return (
-    <div className='rich-editor'>
+  const saveContent = () => {
+    props.saveContent(
+      JSON.stringify(convertToRaw(editorState.getCurrentContent())),
+      contentHasChanged
+    );
+    setEditorState(EditorState.createEmpty());
+  };
+
+  let styleControls = null;
+  if (!readOnly) {
+    styleControls = (
       <div className='rich-editor__style-controls'>
         <InlineStyleControls
-          editorState={props.editorState}
-          onToggle={props.toggleInlineStyle}
+          editorState={editorState}
+          onToggle={toggleInlineStyle}
         />
         <BlockStyleControls
-          editorState={props.editorState}
-          onToggle={props.toggleBlockType}
+          editorState={editorState}
+          onToggle={toggleBlockType}
         />
       </div>
-      <div className='rich-editor__editor' onClick={focus}>
+    );
+  }
+
+  let classNames = ['rich-editor'];
+  let editorClassNames = ['rich-editor__editor'];
+  if (readOnly) {
+    classNames.push('rich-editor--read-only');
+    editorClassNames.push('rich-editor__editor--read-only');
+  }
+
+  let buttonContent = null;
+  if (!readOnly) {
+    let button = null;
+    if (type === THREAD) {
+      button = 'Post';
+      classNames.push('rich-editor--thread');
+    } else if (type === REPLY) {
+      button = 'Reply';
+      classNames.push('rich-editor--reply');
+    }
+    buttonContent = (
+      <button onClick={saveContent} className='rich-editor__reply'>
+        {button}
+      </button>
+    );
+  }
+
+  return (
+    <div className={classNames.join(' ')}>
+      {styleControls}
+      <div className={editorClassNames.join(' ')} onClick={focus}>
         <Editor
-          editorState={props.editorState}
-          handleKeyCommand={props.handleKeyCommand}
-          onTab={props.onTab}
-          onChange={props.onChange}
+          editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          onTab={onTab}
+          onChange={onChange}
           placeholder='Type here...'
           ref={editor}
+          readOnly={readOnly}
         />
       </div>
-      <button className='rich-editor__reply'>Reply</button>
+      {buttonContent}
     </div>
   );
 };
