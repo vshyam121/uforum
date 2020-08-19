a47198fea296953fba438467fa4aba70b38b5107
vshyam121 <v.shyam121@gmail.com>
2020-08-18 23:25:28 -0700

    Using new containers instead of components

--- src/App.js
+++ src/App.js
@@ -5,32 +5,39 @@ import ThreadContainer from './containers/ThreadContainer';
 import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { authenticateToken } from './store/auth/actions';
-import { getAllForums } from './store/forum/actions';
-import NewThread from './components/NewThread/NewThread';
+import { getAllForums } from './store/feeds/actions';
 import SignInContainer from './containers/SignInContainer';
 import SignUpContainer from './containers/SignUpContainer';
-import UserProfile from './components/UserProfile/UserProfile';
+import NewThreadContainer from './containers/NewThreadContainer';
+import UserProfileContainer from './containers/UserProfileContainer';
+import DashboardContainer from './containers/DashboardContainer';
 
 function App(props) {
+  const { authenticateToken, getAllForums } = props;
   useEffect(() => {
-    props.authenticateToken();
-    props.getAllForums();
-  });
+    authenticateToken();
+    getAllForums();
+  }, [authenticateToken, getAllForums]);
 
   return (
     <Layout>
       <Switch>
         <Route exact path='/signin' component={SignInContainer} />
         <Route exact path='/signup' component={SignUpContainer} />
+        <Route exact path='/admin' component={DashboardContainer} />
         <Route exact path='/' component={ForumFeedContainer} />
         <Route exact path='/:forumSlug' component={ForumFeedContainer} />
-        <Route exact path='/user/:username' component={UserProfile} />
+        <Route exact path='/user/:username' component={UserProfileContainer} />
         <Route
           exact
           path='/:forumSlug/thread/:threadSlug'
           component={ThreadContainer}
         />
-        <Route exact path='/:forumSlug/new-thread' component={NewThread} />
+        <Route
+          exact
+          path='/:forumSlug/new-thread'
+          component={NewThreadContainer}
+        />
       </Switch>
     </Layout>
   );
