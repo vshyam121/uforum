56c2cb38f9bb643ba311be7fa4bec9c64c31c5fb
vshyam121 <v.shyam121@gmail.com>
2020-08-18 23:22:04 -0700

    Add support for thread sorting, loading/error handling

--- src/containers/ForumFeedContainer.js
+++ src/containers/ForumFeedContainer.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import {
-  getUnpinnedThreads,
-  getPinnedThreads,
-  setCurrentThread,
-} from '../store/forum/actions';
+import { getUnpinnedThreads, getPinnedThreads } from '../store/feeds/actions';
+import { setCurrentThread } from '../store/thread/actions';
 import { useParams } from 'react-router';
 import ForumFeed from '../components/ForumFeed/ForumFeed';
 import find from 'lodash/find';
@@ -12,69 +9,68 @@ import find from 'lodash/find';
 const ForumFeedContainer = (props) => {
   const currentForumSlug = useParams().forumSlug;
 
-  const {
-    forums,
-    unpinnedThreads,
-    gettingUnpinnedThreads,
-    pinnedThreads,
-    gettingPinnedThreads,
-    getUnpinnedThreads,
-    getPinnedThreads,
-    setCurrentThread,
-  } = props;
+  const { forums, getUnpinnedThreads, getPinnedThreads } = props;
 
-  const [forumFound, setForumFound] = useState(true);
+  const [forumFound, setForumFound] = useState(false);
   const [forumSlug, setForumSlug] = useState(null);
+  const [forumId, setForumId] = useState(null);
+  const [sortingMethod, setSortingMethod] = useState('date');
 
   useEffect(() => {
     let forumId = null;
-    if (forums && forums.length > 0) {
+    if (forums) {
       if (currentForumSlug) {
         const forum = find(forums, { slug: currentForumSlug });
         if (forum) {
           forumId = forum._id;
           setForumSlug(forum.slug);
         }
-      } else {
+      } else if (forums.length > 0) {
         forumId = forums[0]._id;
         setForumSlug(forums[0].slug);
       }
+
       if (!forumId) {
         setForumFound(false);
       } else {
         setForumFound(true);
+        setForumId(forumId);
+        setSortingMethod('date');
         getUnpinnedThreads(forumId);
         getPinnedThreads(forumId);
       }
     }
   }, [forums, currentForumSlug, getUnpinnedThreads, getPinnedThreads]);
 
-  console.log(forumSlug);
-  let forumFeed = null;
-  if (forumFound) {
-    forumFeed = (
-      <ForumFeed
-        forums={forums}
-        unpinnedThreads={unpinnedThreads}
-        gettingUnpinnedThreads={gettingUnpinnedThreads}
-        forumSlug={forumSlug}
-        pinnedThreads={pinnedThreads}
-        gettingPinnedThreads={gettingPinnedThreads}
-        setCurrentThread={setCurrentThread}
-      />
-    );
-  }
+  const handleGetThreads = (sortingMethod) => {
+    getUnpinnedThreads(forumId, sortingMethod);
+  };
 
-  return forumFeed;
+  return (
+    <ForumFeed
+      {...props}
+      forums={forums}
+      forumFound={forumFound}
+      sortingMethod={sortingMethod}
+      forumSlug={forumSlug}
+      handleGetThreads={handleGetThreads}
+      setSortingMethod={setSortingMethod}
+    />
+  );
 };
 
 const mapStateToProps = (state) => ({
-  forums: state.forum.forums,
-  threads: state.forum.threads,
-  pinnedThreads: state.forum.pinnedThreads,
-  gettingPinnedThreads: state.forum.gettingPinnedThreads,
-  unpinnedThreads: state.forum.unpinnedThreads,
-  gettingUnpinnedThreads: state.forum.gettingUnpinnedThreads,
+  forums: state.feeds.forums,
+  gettingForums: state.feeds.gettingForums,
+  getForumsError: state.feeds.getForumsError,
+
+  gettingUnpinnedThreads: state.feeds.gettingUnpinnedThreads,
+  unpinnedThreads: state.feeds.unpinnedThreads,
+  getUnpinnedThreadsError: state.feeds.getUnpinnedThreadsError,
+
+  gettingPinnedThreads: state.feeds.gettingPinnedThreads,
+  pinnedThreads: state.feeds.pinnedThreads,
+  getPinnedThreadsError: state.feeds.getPinnedThreadsError,
 });
 export default connect(mapStateToProps, {
   getUnpinnedThreads,
