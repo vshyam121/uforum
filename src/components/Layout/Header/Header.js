8a228596abc2f202206c71579c7c3fd4119cb881
vshyam121 <v.shyam121@gmail.com>
2020-08-18 23:19:27 -0700

    Add support for dashboard and user profile

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,50 +1,67 @@
 import React, { useState } from 'react';
 import './Header.scss';
 import UForumLogo from '../../../images/uforum.png';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { FaUserCircle } from 'react-icons/fa';
 import DropDownMenu from '../../Theme/DropDownMenu/DropDownMenu';
 import { signOut } from '../../../store/auth/actions';
 
 const Header = (props) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const { user } = props;
+
+  const handleMenuToggle = () => {
+    setShowMenu((prevShowMenu) => !prevShowMenu);
+  };
+
+  const handleClickDashboard = () => {
+    props.history.push('/admin');
+  };
+
+  const handleClickUserProfile = () => {
+    props.history.push(`/user/${user.username}`);
+  };
+
+  const handleClickSignOut = () => {
+    props.signOut();
+  };
+
   let userInfo = (
     <Link className='header__auth link' to='/signin'>
       <h2>Sign up / Sign in</h2>
     </Link>
   );
 
-  const [showMenu, setShowMenu] = useState(false);
-
-  const handleMenuToggle = () => {
-    setShowMenu((prevShowMenu) => !prevShowMenu);
-  };
-
-  const handleSignOut = () => {
-    props.signOut();
-  };
-
-  if (props.user) {
+  if (user) {
+    let dashboard = null;
+    if (user.role === 'admin') {
+      dashboard = (
+        <div className='header__menu-item' onClick={handleClickDashboard}>
+          <span>Dashboard</span>
+        </div>
+      );
+    }
     userInfo = (
       <div className='header__menu' onClick={handleMenuToggle}>
         <div className='header__user'>
           <FaUserCircle className='header__usericon' />
-          <div className='header__username'>{props.user.username}</div>
+          <div className='header__username'>{user.username}</div>
         </div>
         <DropDownMenu show={showMenu}>
-          <Link
-            className='header__profile link'
-            to={`/user/${props.user.username}`}
-          >
-            My Profile
-          </Link>
-          <button className='header__signout' onClick={handleSignOut}>
+          {dashboard}
+          <div className='header__menu-item' onClick={handleClickUserProfile}>
+            <span>My Profile</span>
+          </div>
+          <div className='header__menu-item' onClick={handleClickSignOut}>
             <span>Sign Out</span>
-          </button>
+          </div>
         </DropDownMenu>
       </div>
     );
   }
+
   return (
     <header className='header'>
       <Link to='/' className='header__logo-title link'>
@@ -60,4 +77,4 @@ const mapStateToProps = (state) => ({
   user: state.auth.user,
 });
 
-export default connect(mapStateToProps, { signOut })(Header);
+export default connect(mapStateToProps, { signOut })(withRouter(Header));
