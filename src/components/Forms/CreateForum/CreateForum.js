409ebc93d1a980feae7e4ae94f2e15cd1b8897e8
vshyam121 <v.shyam121@gmail.com>
2020-08-18 23:24:57 -0700

    Creating forum for admins container/components

---
+++ src/components/Forms/CreateForum/CreateForum.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './CreateForum.scss';
+import { ClipLoader } from 'react-spinners';
+import Form from '../../Theme/Form/Form';
+import PropTypes from 'prop-types';
+
+/* Create a form */
+const CreateForum = (props) => {
+  let form = (
+    <Form
+      {...props}
+      onSubmit={props.handleSubmit}
+      updateForm={props.updateForm}
+    />
+  );
+  if (props.creatingForum) {
+    form = (
+      <div className='create-forum__loading'>
+        <ClipLoader size={50} />
+      </div>
+    );
+  }
+
+  let errorMessage = null;
+  if (props.error) {
+    errorMessage = <div className='create-forum__error'>{props.error}</div>;
+  }
+
+  return (
+    <div className='create-forum'>
+      <h2 className='create-forum__title'>Create a forum</h2>
+      <div className='form-component'>
+        {errorMessage}
+        {form}
+      </div>
+    </div>
+  );
+};
+
+CreateForum.propTypes = {
+  creatingForum: PropTypes.bool,
+  error: PropTypes.object,
+  isAuthenticated: PropTypes.string,
+};
+
+export default CreateForum;
---
+++ src/containers/CreateForumContainer.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { createForum, resetCreateForum } from '../store/feeds/actions';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import CreateForum from '../components/Forms/CreateForum/CreateForum';
+
+/* Create forum form */
+class CreateForumContainer extends Component {
+  initialForm = {
+    forumName: {
+      elementType: 'input',
+      elementConfig: {
+        placeholder: 'Forum Name',
+      },
+      value: '',
+      errorMessage:
+        'Forum name can only contain letters. Min 4 chars and max 15 chars.',
+      validation: {
+        required: true,
+        isName: true,
+        minLength: 4,
+        maxLength: 15,
+      },
+      valid: false,
+    },
+  };
+
+  state = {
+    form: this.initialForm,
+    formIsValid: false,
+    formSubmitted: false,
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ formSubmitted: true });
+    if (this.state.formIsValid) {
+      this.props.createForum(this.state.form.forumName.value);
+    }
+  };
+
+  componentDidUpdate() {
+    if (this.props.createdForum) {
+      this.setState({
+        form: this.initialForm,
+        formSubmitted: false,
+        formIsValid: false,
+      });
+    }
+    this.props.resetCreateForum();
+  }
+
+  updateForm = (stateUpdate) => {
+    this.setState(stateUpdate);
+  };
+
+  render() {
+    return (
+      <CreateForum
+        {...this.state}
+        {...this.props}
+        handleSubmit={this.handleSubmit}
+        updateForm={this.updateForm}
+      />
+    );
+  }
+}
+
+CreateForumContainer.propTypes = {
+  error: PropTypes.object,
+};
+
+const mapStateToProps = (state) => ({
+  error: state.feeds.createForumError,
+  createdForum: state.feeds.createdForum,
+  creatingForum: state.feeds.creatingForum,
+});
+
+export default connect(mapStateToProps, { createForum, resetCreateForum })(
+  CreateForumContainer
+);
