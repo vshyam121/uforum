6935cf61b20da652c263895656058beb50fb488e
vshyam121 <v.shyam121@gmail.com>
2020-08-22 20:12:25 -0700

    Updates

---
+++ precache-manifest.ff213a22594692e381ece12b9a736258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f690d618d18cb1335f38beeded0290f",
+    "url": "/uforum/index.html"
+  },
+  {
+    "revision": "339f628353e29c039239",
+    "url": "/uforum/static/css/main.fa8ab9dd.chunk.css"
+  },
+  {
+    "revision": "862afe7cebb9ae21b9ba",
+    "url": "/uforum/static/js/2.3a1fb224.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/uforum/static/js/2.3a1fb224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "339f628353e29c039239",
+    "url": "/uforum/static/js/main.ce676348.chunk.js"
+  },
+  {
+    "revision": "6b98f8f7f4b020d5035e",
+    "url": "/uforum/static/js/runtime-main.f090c8b6.js"
+  }
+]);
