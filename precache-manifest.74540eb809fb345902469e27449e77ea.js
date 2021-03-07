76939efe7d98f65cf67763c7e7feaf702f075b79
Shyam Vijayakumar <v.shyam121@gmail.com>
2021-03-06 20:38:24 -0800

    Updates

---
+++ precache-manifest.74540eb809fb345902469e27449e77ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b576d55e1b95c4acfd561e1df5f2daef",
+    "url": "/uforum/index.html"
+  },
+  {
+    "revision": "355fac66a13d030ac8d7",
+    "url": "/uforum/static/css/main.8194f5d1.chunk.css"
+  },
+  {
+    "revision": "862afe7cebb9ae21b9ba",
+    "url": "/uforum/static/js/2.3a1fb224.chunk.js"
+  },
+  {
+    "revision": "702e06c21f4db63e7e05a8a0eda9baf1",
+    "url": "/uforum/static/js/2.3a1fb224.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355fac66a13d030ac8d7",
+    "url": "/uforum/static/js/main.7616c37e.chunk.js"
+  },
+  {
+    "revision": "6b98f8f7f4b020d5035e",
+    "url": "/uforum/static/js/runtime-main.f090c8b6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b2c1f33e854a561c5dc37992eda7dcac.js"
+  "/uforum/precache-manifest.74540eb809fb345902469e27449e77ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/uforum/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
